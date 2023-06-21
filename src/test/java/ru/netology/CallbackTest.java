a05cd17c517cf52a1b8ad0da1c9acfa2bc3fe967
Spider-Dad <127666929+Spider-Dad@users.noreply.github.com>
2023-06-22 00:50:15 +0300

    Update CallbackTest.java

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -28,7 +28,7 @@ public class CallbackTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+        // options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver = new ChromeDriver();
     }
