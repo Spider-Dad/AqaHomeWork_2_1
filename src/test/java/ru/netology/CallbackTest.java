442eb6b2b6b46b3863eeeaca3bebfd4fd1cc29c5
Pavel Shvydkin <Pshvydkin@gmail.com>
2023-06-21 22:37:29 +0300

    modified CallbackTest

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,15 +1,24 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CallbackTest {
     private WebDriver driver;
-
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
@@ -29,6 +38,18 @@ public class CallbackTest {
     @Test
     void shouldCallbackTest(){
         // загрузка страницы
-        driver.get("http://0.0.0.0:9999");
+        driver.get("http://192.168.1.77:9999");
+
+//        WebElement form = driver.findElement(By.className("input__control"));
+//        form.findElement((By.cssSelector("[data-test-id=name] input"))).sendKeys("Вася Пупкин");
+//        form.findElement((By.cssSelector("[data-test-id=phone] input"))).sendKeys("+79991234567");
+        driver.findElement((By.cssSelector("[data-test-id=name] input"))).sendKeys("Вася Пупкин");
+        driver.findElement((By.cssSelector("[data-test-id=phone] input"))).sendKeys("+79991234567");
+        driver.findElement((By.cssSelector("[data-test-id=agreement]"))).click();
+        driver.findElement(By.cssSelector("button.button.button_view_extra.button_size_m.button_theme_alfa-on-white")).click();
+        String text = driver.findElement((By.cssSelector("[data-test-id=order-success]"))).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
+
+
     }
 }
